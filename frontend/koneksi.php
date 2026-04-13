52b1e8ba493370ab389acd75814e143cdc40440a
n4ctbyte <christiannakata9392@gmail.com>
2026-04-14 00:33:47 +0700

    configure vercel for mysql backend

--- frontend/koneksi.php
+++ frontend/koneksi.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_undangan";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die(mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
